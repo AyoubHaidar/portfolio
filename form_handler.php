e7a7e78149d5f5256f69f8a00f9c63bf71affda5
ayoub <ayoub.haidar5250@gmail.com>
2020-06-19 00:37:34 +0300

    add code to php

--- form_handler.php
+++ form_handler.php
@@ -1,5 +1,18 @@
 <?php 
+$name = $_POST ['name'];
+$email = $_POST ['email'];
+$message = $_POST ['message'];
 
+$email_from = 'ayoub@it-lb.co';
+$email_sbg = "new form";
+$email_body = "user name: $name. \n"
+."user email: $email. \n"
+."user message: $message.\n";
 
+$to = "ayoub.haidar5250@gmail.com";
+$headers = "from: $email_from \r\n";
+$headers .= "reply-to $email \r\n";
+mail($to,$email_sbg,$email_body,$headers)
+header(Location: index.html);
 
 ?>
